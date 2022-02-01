4424635addb8c0eef1eaf82d831a4d990f87be06
Lotfi-Touil <lotfi.touil7@gmail.com>
2022-02-01 14:35:45 +0100

    récupérer de la vie

--- Assets/Script/LightRadius.cs
+++ Assets/Script/LightRadius.cs
@@ -7,12 +7,24 @@ public class LightRadius : MonoBehaviour
 {
     public float timeStart = 60;
     UnityEngine.Experimental.Rendering.Universal.Light2D playerLight;
-    // Start is called before the first frame update
+    
+    public static LightRadius instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de PlayerMovement dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+
     void Start()
     {
         playerLight = GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
     }
-
     void FixedUpdate()
     {
         if (playerLight.pointLightOuterRadius > 1)
@@ -25,7 +37,12 @@ public class LightRadius : MonoBehaviour
         }
     }
     void Die(){
-        PlayerMovement.instance.animator.SetTrigger("Die");
+         PlayerMovement.instance.animator.SetTrigger("Die");
          PlayerMovement.instance.enabled = false;
+    }
+
+    public void aggrandirHallo()
+    {
+        playerLight.pointLightOuterRadius += 0.09766666666f * 5;
     }
 }
