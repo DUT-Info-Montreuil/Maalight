2e0de001d8094340016aa05ea0d96288e7d665dc
Annaël Moussa <annaelmoussa@gmail.com>
2022-02-01 01:25:04 +0100

    Ajout animation perso + halo de lumiere

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,30 +2,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public bool isJumping = false;
+    public bool isJumping;
+    public bool isGrounded;
     public float moveSpeed;
     public float jumpForce;
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
     void Update ()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             isJumping = true;
         }
     }
-    // Update is called once per frame
+
     void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         MovePlayer(horizontalMovement);
+        Flip(rb.velocity.x);
+        float characterVelocity = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("Speed",characterVelocity);
     }
+
     void MovePlayer(float _horizontalMovement){
         Vector3 targetVelocity = new Vector2(_horizontalMovement,rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
         if(isJumping == true){
             rb.AddForce(new Vector2(0f,jumpForce));
             isJumping = false;
+        }
+    }
+    void Flip(float _velocity){
+        if (_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }else if(_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
         }
     }
 }
